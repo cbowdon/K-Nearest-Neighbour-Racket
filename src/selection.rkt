ca469389bb148920d708ebd3636e5e42c11de531
Chris Bowdon <c.bowdon@gmail.com>
2013-06-21 21:17:29 +0100

    Made quick-sort1 use values.

--- src/selection.rkt
+++ src/selection.rkt
@@ -1,5 +1,7 @@
 #lang typed/racket/base
 
+(require racket/list)
+
 (provide quick-sort1)
 
 (define-type Sort (All (a) ((Listof a) (a a -> Boolean) -> (Listof a))))
@@ -8,12 +10,13 @@
 (define (nth-element lst comp)
   '())
 
-(: quick-sort1 (All (a) ((Listof a) (a a -> Boolean) Integer -> (Listof a))))
+; turns out racket/list has partition, using that instead
+(: quick-sort1 (All (a) ((Listof a) (a a -> Boolean) Integer -> (Values (Listof a) (Listof a)))))
 (define (quick-sort1 lst comp pivot)
   (let ([p (list-ref lst pivot)])
-	(: qs-iter ((Listof a) (Listof a) (Listof a) -> (Listof a)))
+	(: qs-iter ((Listof a) (Listof a) (Listof a) -> (Values (Listof a) (Listof a))))
 	(define (qs-iter in less greater)
-	  (cond [(null? in) (append less greater)]
+	  (cond [(null? in) (values less greater)]
 			[(comp (car in) p) (qs-iter (cdr in) (cons (car in) less) greater)]
-			[(qs-iter (cdr in) less (cons (car in) greater))]))
+			[else (qs-iter (cdr in) less (cons (car in) greater))]))
 	(qs-iter lst '() '())))
--- src/test.rkt
+++ src/test.rkt
@@ -5,17 +5,19 @@
 
 (test-case
   "1 iteration of quick-sort"
-  (let ([result (quick-sort1 (list 2.0 3.0 5.0 8.0 4.0 1.0 9.0 0.1 4.0) < 4)])
-	(check-equal?
-	  (list-ref result 4)
-	  4.0)
-	(check-equal?
-	  (take result 4)
-	  (list 0.1 1.0 3.0 2.0))
-	(check-equal?
-	  (drop result 4)
-	  (list 4.0 9.0 4.0 8.0 5.0))))
+  (let-values ([(lt gt) (quick-sort1 (list 2.0 3.0 5.0 8.0 4.0 1.0 9.0 0.1 4.0) < 4)])
+	(check-equal? (car gt) 4.0)
+	(check-equal? lt (list 0.1 1.0 3.0 2.0))
+	(check-equal? gt (list 4.0 9.0 4.0 8.0 5.0))))
 
 (test-case
   "quick-sort1 on a 1-element list"
-  (check-equal? (quick-sort1 (list 1.0) < 0) (list 1.0)))
+  (let-values ([(lt gt) (quick-sort1 (list 1.0) < 0)])
+	(check-equal? lt '())
+	(check-equal? gt (list 1.0))))
+
+(test-case
+  "partition from racket/list - just for interest"
+  (let-values ([(lss gtr) (partition (lambda (x) (< x 4.0)) (list 2.0 3.0 5.0 8.0 4.0 1.0 9.0 0.1 4.0))])
+	(check-equal? lss (list 2.0 3.0 1.0 0.1))
+	(check-equal? gtr (list 5.0 8.0 4.0 9.0 4.0))))
