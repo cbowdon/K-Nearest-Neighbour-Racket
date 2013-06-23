f4d016e313bbee60899861ad0b1cc154aa7d5804
Chris Bowdon <c.bowdon@gmail.com>
2013-06-23 09:40:56 +0100

    Finished quick-select.

--- src/selection.rkt
+++ src/selection.rkt
@@ -4,12 +4,12 @@
 		 racket/function)
 
 (provide quick-sort1
-		 ;quick-select
+		 quick-select
 		 nth-element)
 
-(: nth-element (All (a) ((Listof a) Index (a a -> Boolean) -> (Listof a))))
-(define (nth-element lst n comp)
-  '())
+(: nth-element (All (a) ((Listof a) Index (a -> Real) -> (Listof a))))
+(define (nth-element lst n sel)
+  (quick-select lst n sel))
 
 (: quick-sort1 (All (a) ((Listof a) Index (a -> Real) -> (Values (Listof a) (Listof a) (Listof a)))))
 (define (quick-sort1 lst pivot sel)
@@ -22,7 +22,7 @@
 			[(> (sel (car in)) (sel p))
 			 (qs-iter (cdr in) less eq (cons (car in) greater))]
 			[else 
-			 (qs-iter (cdr in) less (cons (car in) eq) greater)]))
+			  (qs-iter (cdr in) less (cons (car in) eq) greater)]))
 	(qs-iter lst '() '() '())))
 
 (: quick-select (All (a) ((Listof a) Index (a -> Real) -> (Listof a))))
@@ -31,51 +31,12 @@
   (define (qs-iter working lower higher)
 	(define p (quotient (length working) 2))
 	(define-values (lt eq gt) (quick-sort1 working p sel))
-	(define pivot-new-index (length lt))
+	(define pivot-new-index (+ (length lt) (length lower)))
 	(cond [(= pivot-new-index n)
-		   (begin
-			 (printf "equal\n")
-			 (append lt eq gt))]
+		   (append lower lt eq gt higher)]
 		  [(> pivot-new-index n)
-		   (begin
-			 (printf "lt\n")
-			 (qs-iter lt lower (append eq gt higher)))]
-		  [else
-		   (begin
-			 (printf "gt\n")
-			 (qs-iter (append eq gt) (append lower lt) higher))]))
-  '())
-
-; nth element is index 2
-; should be 3
-(: n Real)
-(define n 2)
-
-(: data (Listof Real))
-(define data (list 3 2 1 6 5 7 8 4 9))
-
-(: ident (All (a) (a -> Real)))
-(define (ident x) (cast x Real))
-
-(define-values (lt eq gt) (quick-sort1 data 4 ident))
-
-;(partition (lambda: ([x : Real]) (< x (list-ref data 4))) data)
-
-; 4th element is in its correct place - as head of gt list
-(define pivot-new-index (length lt))
-
-(printf "~a ~a ~a\n" lt eq gt)
-
-
-(cond [(= pivot-new-index n)
-	   (begin
-		 (printf "equal\n")
-		 (values lt eq gt))]
-	  [(> pivot-new-index n)
-	   (begin
-		 (printf "lt\n")
-		 (quick-sort1 lt 2 ident))]
-	  [else
-	   (begin
-		 (printf "gt\n")
-		 (quick-sort1 (append eq gt) 6 ident))])
+		   (qs-iter lt lower (append eq gt higher))]
+		  [(> (+ pivot-new-index (length eq)) n)
+		   (append lower lt eq gt higher)]
+		  [else (qs-iter (append eq gt) (append lower lt) higher)]))
+  (qs-iter lst '() '()))
